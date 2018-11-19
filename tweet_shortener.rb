7c890d2980bc28540d2a52687d9011906b9de8a5
Jason Stone <jason.stone0808@gmail.com>
2018-11-19 22:41:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -21,12 +21,8 @@ def word_substituter(tweet)
   end
 end
 
-def word_substituter(tweet)
-  tweet.split.collect do |word|
-    if dictionary.keys.include?(word.downcase)
-      word = dictionary[word.downcase]
-    else
-      word
-    end
-  end.join(" ")
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
 end
