5a65fa48b63530c645d436509b14c23b21e93534
Jason Stone <jason.stone0808@gmail.com>
2018-11-19 21:51:40 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,7 +4,7 @@ def dictionary
   h = {"hello" => "hi", "to, two, too" => "why", "for, four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
 end
 
-def word_substituter(tweets)
+def word_substituter(tweet_one)
   tweet_one.gsub(/\w+/, ) do |m|
     dictionary.fetch(m,m)
   end
